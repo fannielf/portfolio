0982d45da23d793ce69f7639a2080af4d663433c
Roope Hongisto <roope@Roopes-MacBook-Air.local>
2025-10-18 12:48:42 +0300

    portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // enables manual dark mode toggle
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
