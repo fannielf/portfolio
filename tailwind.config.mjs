00c77b78d91e20f19650fc6e14e46e1c07925062
fannielf <fanni.vesanen@gritlab.ax>
2025-11-23 14:55:45 +0200

    static

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // enables manual dark mode toggle
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
